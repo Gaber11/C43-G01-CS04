23d6086fbe67ebcfd0230bc0bc4c8333f7f6198c
Gaber11 <gaberezzat0@gmail.com>
2024-12-11 20:54:58 +0200

    1- Write a program in C# Sharp to merge two arrays of the same size sorted in ascending order.

---
+++ Assignment/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1- Write a program in C# Sharp to merge two arrays of the same size sorted in ascending order.
+          //int[] arr1 = { 10,20,30};
+          //int[] arr2 = { 40,50,60};
+          //int[]arr3= new int[arr1.Length +arr2.Length];
+         
+          //  Array.Copy(arr1,arr3,3);
+          //  Array.ConstrainedCopy(arr2,0,arr3,3,3);
+
+          //  Array.Sort(arr3);
+
+          //  for (int i = 0; i < arr3.Length; i++)
+          //  {
+          //      Console.WriteLine(arr3[i]);
+          //  }
+            #endregion
+
+
+
+
+        }
+    }
+}
