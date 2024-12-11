f046b80bf4a28b968bd6e64a3bd9319023f1d831
Gaber11 <gaberezzat0@gmail.com>
2024-12-11 11:26:20 +0200

    1 - Judged Array

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,11 +4,52 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
 
 
+            #region 1-Judged Array
+            //int[][]marks=  new int[3][];
+            //marks[0] = new int[3] { 10, 20, 30 };
+            //marks[1] = new int[2] { 10, 20};
+            //marks[2] = new int[1] { 50 };
 
+            //for (int i = 0; i < marks.Length; i++)
+            //{
+            //    for (int j = 0; j < marks[i].Length; j++)
+            //    {
+            //        Console.Write(marks[i][j] + "  ");
 
-        }
+            //    }
+            //    Console.WriteLine();
+            //}
+                #endregion
+
+                #region
+                #endregion
+
+                #region
+                #endregion
+
+                #region
+                #endregion
+
+                #region
+                #endregion
+
+                #region
+                #endregion
+
+                #region
+                #endregion
+
+                #region
+                #endregion
+
+                #region
+                #endregion
+
+                #region
+                #endregion
+
+            }
     }
 }
