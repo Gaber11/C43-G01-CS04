6ec8a110cf9936ca2482ac4293e1d1959cbffa15
Gaber11 <gaberezzat0@gmail.com>
2024-12-11 21:26:18 +0200

    2- Write a program in C# Sharp to count the frequency of each element of an array.

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -5,23 +5,72 @@
         static void Main(string[] args)
         {
             #region 1- Write a program in C# Sharp to merge two arrays of the same size sorted in ascending order.
-          //int[] arr1 = { 10,20,30};
-          //int[] arr2 = { 40,50,60};
-          //int[]arr3= new int[arr1.Length +arr2.Length];
-         
-          //  Array.Copy(arr1,arr3,3);
-          //  Array.ConstrainedCopy(arr2,0,arr3,3,3);
+            //int[] arr1 = { 10,20,30};
+            //int[] arr2 = { 40,50,60};
+            //int[]arr3= new int[arr1.Length +arr2.Length];
 
-          //  Array.Sort(arr3);
+            //  Array.Copy(arr1,arr3,3);
+            //  Array.ConstrainedCopy(arr2,0,arr3,3,3);
 
-          //  for (int i = 0; i < arr3.Length; i++)
-          //  {
-          //      Console.WriteLine(arr3[i]);
-          //  }
+            //  Array.Sort(arr3);
+
+            //  for (int i = 0; i < arr3.Length; i++)
+            //  {
+            //      Console.WriteLine(arr3[i]);
+            //  }
             #endregion
 
+            #region 2- Write a program in C# Sharp to count the frequency of each element of an array.
+           // int[] arr = { 1, 2, 3, 4, 4, 5, 4, 5, 6, 6 };
+           //int freq = 0;
+           // for (int i = 0; i < arr.Length; i++)
+           // {
+           //     Console.Write(arr[i]);  
+              
+           //     for (int j = 0;j < arr.Length; j++)
+           //     {
+           //         if (arr[i] == arr[j])
+           //             freq+= arr[i];
+           //         Console.Write(freq);
 
+           //     }
+           //     Console.WriteLine();
 
+           // }
+           //Don't know.
+
+            #endregion
+
+            #region
+
+            #endregion
+
+            #region
+            #endregion
+
+            #region
+            #endregion
+
+            #region
+            #endregion
+
+            #region
+            #endregion
+
+            #region
+            #endregion
+
+            #region
+            #endregion
+
+            #region
+            #endregion
+
+            #region
+            #endregion
+
+            #region
+            #endregion
 
         }
     }
