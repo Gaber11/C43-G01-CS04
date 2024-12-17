2d50971dc35af91e01c86f47d45fba58fd30ac7b
Gaber11 <gaberezzat0@gmail.com>
2024-12-17 14:50:57 +0200

    1-Explain the difference between passing (Value type parameters) by value and by reference then write a suitable c# example.

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -230,7 +230,21 @@
             //===============================>Function<=====================================================
             #region 1-Explain the difference between passing (Value type parameters) by value and by reference then write a suitable c# example.
 
-            //function لساا محتاج اذاككرها أكتر قبل ما احلها لان عطلت ف حاجات كتير
+            //When you pass a value type by value, a copy of the variable is passed to the method.
+            //changes made to the parameter inside the method do not affect the original variable.
+            //int num1 = 4;
+            //int num2 = 5;
+
+            //Console.WriteLine(num1); //4
+            //Printvaluetype(num1); //7
+            //Console.WriteLine(num1); //4 
+            ////===============================
+            //Console.WriteLine(num2);  //5
+            //PrintRefrencetype(ref num2); //9
+            //Console.WriteLine(num2);    //9
+
+
+
             #endregion
 
             #region
@@ -244,5 +258,17 @@
 
         }
 
+        #region Q1)function
+        //static void Printvaluetype(int num1)
+        //{
+        //    num1 = num1 + 3;
+        //    Console.WriteLine(num1);
+        //}
+        //static void PrintRefrencetype(ref int num2)
+        //{
+        //    num2 = num2 + 4;
+        //    Console.WriteLine(num2);
+        //}
+        #endregion
     }
 }
